287f73611c037d8725810591b50e6c4ae662ff3a
Gregory-Kimaru-K <gregorykariara1@gmail.com>
2025-02-28 10:49:07 +0300

    contexts done

--- frontend/contexts/AuthContext.tsx
+++ frontend/contexts/AuthContext.tsx
@@ -1,10 +1,11 @@
-import React, { createContext, ReactNode, useState } from "react";
+import React, { createContext, ReactNode } from "react";
+import backendURL from "@/constants/constants";
 
 // Create Context
 interface authContext{
-    forgotPassword: () => void,
-    login: () => void,
-    confirmCode: () => void,
+    forgotPassword: (forgData: {email: string}) => void,
+    login: (loginData: {email: string, code: string}) => void,
+    confirmCode: (confirmData: {email: string, code: string}) => void,
     logout: () => void
 }
 const AuthContext = createContext<undefined | authContext>(undefined);
@@ -13,9 +14,49 @@ export default AuthContext;
 
 // AuthProvider Component
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-    const forgotPassword = () => {}
-    const login = () => {}
-    const confirmCode =() => {}
+    const forgotPassword = async(forgData: {email: string}) => {
+        try{
+            const response = await fetch(`${backendURL}validate/forgot/`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(forgData)
+            })
+        }
+        catch (error){
+            console.log(`MJR_ERROR_FORGOT: ${error}`)
+        }
+    }
+
+    const login = async(loginData: {email: string, password: string}) => {
+        try{
+            const response = await fetch(`${backendURL}tokens/`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(loginData)
+            })
+        }
+        catch (error){
+            console.log(`MJR_ERROR_LOGIN: ${error}`)
+        }
+    }
+    const confirmCode = async(confirmData: {email: string, code: string}) => {
+        try{
+            const response = await fetch(`${backendURL}validate/code/`,{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(confirmData)
+            })
+        }
+        catch (error){
+            console.log(`MJR_ERROR_CONFIRM_CODE: ${error}`)
+        }
+    }
     const logout = () => {}
 
     const context = {
--- frontend/contexts/UserContext.tsx
+++ frontend/contexts/UserContext.tsx
@@ -1,18 +1,60 @@
 import React, { createContext, ReactNode } from "react";
+import backendURL from "@/constants/constants";
 
 interface userContext {
-    createUser: () => void,
-    updateUser: () => void,
-    emailValidation: () => void
+    createUser: (createData: any) => void,
+    updateUser: (updateData: any, id: number) => void,
+    emailValidation: (emailData: {email: string}) => void
 }
 const UserContext = createContext<undefined | userContext>(undefined)
 
 export default UserContext
 
 export const UserProvider: React.FC<{children: ReactNode}> = ({ children }) => {
-    const emailValidation = () => {}
-    const createUser = () => {}
-    const updateUser = () => {}
+    const emailValidation = async(emailData: {email: string}) => {
+        try{
+            const response = await fetch(`${backendURL}validate/email/`,{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(emailData)
+            })
+        }
+
+        catch (error){
+            console.log(`MJR_ERROR_CONFIRM_CODE: ${error}`)
+        }
+    }
+    const createUser = async(createData: any) => {
+        try{
+            const response = await fetch(`${backendURL}user/new/`,{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(createData)
+            })
+        }
+        catch (error){
+            console.log(`MJR_ERROR_CONFIRM_CODE: ${error}`)
+        }
+    }
+
+    const updateUser = async(updateData: any, id: number) => {
+        try{
+            const response = await fetch(`${backendURL}user/${id}/`,{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(updateData)
+            })
+        }
+        catch (error){
+            console.log(`MJR_ERROR_CONFIRM_CODE: ${error}`)
+        }
+    }
 
     const context = {
         createUser,
